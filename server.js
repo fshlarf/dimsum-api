04a9e6603deaa3596517993ea1d3b441974eee40
ibnuhazar101 <ibnuhazar101@gmail.com>
2023-08-14 09:06:03 +0700

    add more options in express-session cookie

--- server.js
+++ server.js
@@ -96,7 +96,12 @@ Promise.all([
       store: new (connectPgSimple(expressSession))({
         conObject: pgConfig,
       }),
-      cookie: { maxAge: 30 * 24 * 60 * 60 * 1000, sameSite: "none" },
+      cookie: {
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        secure: process.env.NODE_ENV === "development" ? false : true,
+        httpOnly: process.env.NODE_ENV === "development" ? false : true,
+        sameSite: process.env.NODE_ENV === "development" ? false : "none",
+      },
       secret: sessionSecret,
       resave: false,
       saveUninitialized: false,
