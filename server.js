a86b4e2f1d141ae22d27874c43c0c2ed6ad47580
ibnuhazar101 <ibnuhazar101@gmail.com>
2023-08-14 11:11:37 +0700

    hide cookie option

--- server.js
+++ server.js
@@ -98,14 +98,14 @@ Promise.all([
       }),
       cookie: {
         maxAge: 30 * 24 * 60 * 60 * 1000,
-        secure: process.env.NODE_ENV === "development" ? false : true,
+        // secure: process.env.NODE_ENV === "development" ? false : true,
         // httpOnly: process.env.NODE_ENV === "development" ? false : true,
-        sameSite: process.env.NODE_ENV === "development" ? false : "none",
+        // sameSite: process.env.NODE_ENV === "development" ? false : "none",
       },
       secret: sessionSecret,
       resave: false,
       saveUninitialized: false,
-      // sameSite: "none",
+      sameSite: "none",
     })
   );
   app.set("view engine", "ejs");
